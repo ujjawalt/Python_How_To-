4f0638396e4140cad99800e258f93989e1cee4a6
ujjawalt <tanwar896@outlook.com>
2020-01-12 15:52:37 +0530

    Search: Binary Search

---
+++ searching_binary.py
@@ -0,0 +1,34 @@
+"""
+Binary Search-> Simples searching technique. Search entire list one by one.
+"""
+
+pos = -1
+
+
+def search(nums, n):
+
+    lower_bound = 0
+    # If length is 5 then index only go up to 0-4
+    upper_bound = len(nums) - 1
+
+    while lower_bound <= upper_bound:
+        mid = (lower_bound + upper_bound)//2
+
+        if nums[mid] == n:
+            globals()['pos'] = mid
+            return True
+        else:
+            if nums[mid] < n:
+                lower_bound = mid + 1
+            else:
+                upper_bound = mid - 1
+    return False
+
+
+nums = [5, 10, 15, 20, 25, 30, 35, 40]
+n = 30
+
+if search(nums, n):
+    print("Found at ", pos+1)
+else:
+    print("Not Found")
